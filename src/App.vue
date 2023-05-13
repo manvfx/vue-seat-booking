fcdc5bbda519e37b72e6afea3ee3a4e2fb1a3533
manvfx <manvfx@gmail.com>
2023-05-13 10:02:23 +0330

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-// import "roboto-fontface/css/roboto/sass/roboto-fontface-regular.scss";
-// import "roboto-fontface/css/roboto/sass/roboto-fontface-bold.scss";
+import "roboto-fontface/css/roboto/sass/roboto-fontface-regular.scss";
+import "roboto-fontface/css/roboto/sass/roboto-fontface-bold.scss";
 </script>
 
 <template>
   <v-app>
-    <router-view />
+    <router-view></router-view>
   </v-app>
 </template>
 
-<style lang="scss"></style>
